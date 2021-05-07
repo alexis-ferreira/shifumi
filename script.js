4dd30b10df56cce586e33e9e5460d89674c7a4a8
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-05-07 09:55:51 +0200

    Ajout du fichier script

---
+++ script.js
@@ -0,0 +1,215 @@
+'use strict';
+
+let pierre=document.getElementById('pierre').getAttribute('src');
+let feuille=document.getElementById('feuille').getAttribute('src');
+let ciseaux=document.getElementById('ciseaux').getAttribute('src');
+let result=document.getElementById('cpu').getAttribute('src');
+
+let pierreClick=document.getElementById('pierre');
+let feuilleClick=document.getElementById('feuille');
+let ciseauxClick=document.getElementById('ciseaux');
+
+let scoreUser=document.getElementById('userScore').innerHTML;
+// let scoreCpu=document.getElementById('cpuScore').innerHTML;
+
+let h2=document.getElementById("h2").innerHTML;
+
+// console.log(typeof scoreUser);
+
+scoreUser=parseInt(scoreUser);
+
+// console.log(typeof scoreUser);
+
+let userName=prompt("Entrez votre pseudo :");
+
+document.getElementById("user").innerHTML="Score "+userName;
+
+console.log(h2);
+
+console.log(pierreClick);
+
+console.log(pierre);
+console.log(feuille);
+console.log(ciseaux);
+
+let cpu="Robot";
+let cpuChoice;
+
+pierreClick.addEventListener('click',testPierre);
+feuilleClick.addEventListener('click',testFeuille);
+ciseauxClick.addEventListener('click',testCiseaux);
+
+function testPierre(){
+    
+    cpuChoice = (Math.floor(Math.random() * 3 + 1));
+
+    switch(cpu){
+
+        case 1:
+            cpuChoice = "pierre";
+        break;
+    
+        case 2:
+            cpuChoice = "feuille";
+        break;
+    
+        case 3:
+            cpuChoice = "ciseaux";
+        break;
+    }
+
+    console.log(cpuChoice);
+
+    if(cpuChoice == 1){
+        document.getElementById('cpu').src=("/img/pierre.png");
+        document.getElementById("h2").innerHTML="Egalité !";
+        document.getElementById("h2").style.backgroundColor='grey';
+    }
+
+    else{
+
+        switch(pierreClick){
+            case pierreClick:
+                if(cpuChoice == 2){
+                    document.getElementById('cpu').src=("/img/feuille.png");
+                    document.getElementById("h2").innerHTML="Feuille ! "+userName+" a perdu !";
+                    document.getElementById("h2").style.backgroundColor='red';
+                }
+
+                else{
+                    document.getElementById('cpu').src=("/img/ciseaux.png")
+                    document.getElementById("h2").innerHTML="Ciseaux ! "+userName+" a gagné !";
+                    document.getElementById("h2").style.backgroundColor='green';
+                    // document.getElementById("userScore").innerHTML= ++ ;
+                }
+            break;
+        }
+    }
+}
+
+function testFeuille(){
+    
+    cpuChoice = (Math.floor(Math.random() * 3 + 1));
+
+    switch(cpu){
+
+        case 1:
+            cpuChoice = "pierre";
+        break;
+    
+        case 2:
+            cpuChoice = "feuille";
+        break;
+    
+        case 3:
+            cpuChoice = "ciseaux";
+        break;
+    }
+
+    console.log(cpuChoice);
+
+    if(cpuChoice == 2){
+        document.getElementById('cpu').src=("/img/feuille.png");
+        document.getElementById("h2").innerHTML="Egalité !";
+        document.getElementById("h2").style.backgroundColor='grey';
+    }
+
+    else{
+
+        switch(feuilleClick){
+            case feuilleClick:
+                if(cpuChoice == 3){
+                    document.getElementById('cpu').src=("/img/ciseaux.png");
+                    document.getElementById("h2").innerHTML="Ciseaux ! "+userName+" a perdu !";
+                    document.getElementById("h2").style.backgroundColor='red';
+
+
+                }
+
+                else{
+                    document.getElementById('cpu').src=("/img/pierre.png")
+                    document.getElementById("h2").innerHTML="Pierre ! "+userName+" a gagné !";
+                    document.getElementById("h2").style.backgroundColor='green';
+
+
+                }
+            break;
+        }
+    }
+}
+
+
+function testCiseaux(){
+    
+    cpuChoice = (Math.floor(Math.random() * 3 + 1));
+
+    switch(cpu){
+
+        case 1:
+            cpuChoice = "pierre";
+        break;
+    
+        case 2:
+            cpuChoice = "feuille";
+        break;
+    
+        case 3:
+            cpuChoice = "ciseaux";
+        break;
+    }
+
+    console.log(cpuChoice);
+
+    if(cpuChoice == 3){
+        document.getElementById('cpu').src=("/img/ciseaux.png");
+        document.getElementById("h2").innerHTML="Egalité !";
+        document.getElementById("h2").style.backgroundColor='grey';
+    }
+
+    else{
+
+        switch(ciseauxClick){
+            case ciseauxClick:
+                if(cpuChoice == 1){
+                    document.getElementById('cpu').src=("/img/pierre.png");
+                    document.getElementById("h2").innerHTML="Pierre ! "+userName+" a perdu !";
+                    document.getElementById("h2").style.backgroundColor='red';
+
+
+                }
+
+                else{
+                    document.getElementById('cpu').src=("/img/feuille.png")
+                    document.getElementById("h2").innerHTML="Feuille ! "+userName+" a gagné !";
+                    document.getElementById("h2").style.backgroundColor='green';
+
+
+                }
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+// switch(cpu){
+//     case 1:
+//         cpuChoice = "pierre"
+//     break;
+
+//     case 2:
+//         cpuChoice = "feuille"
+//     break;
+
+//     case 3:
+//         cpuChoice = "ciseaux"
+//     break;
+// }
+
+// if
+//     (userChoice == cpuChoice){
+//         document.write("Vous êtes à égalité !")
+//     }
